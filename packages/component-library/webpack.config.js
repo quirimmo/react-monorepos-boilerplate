0fb253392be16bfba9aa46b2a9d9bb0b6a8b36f2
quirimmo <quirimmo@gmail.com>
2019-12-02 22:48:20 +0100

    new version

--- packages/component-library/webpack.config.js
+++ packages/component-library/webpack.config.js
@@ -1,6 +1,7 @@
 const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   mode: 'production',
@@ -13,6 +14,9 @@ module.exports = {
     libraryTarget: 'umd',
     library: 'component-library',
     umdNamedDefine: true
+  },
+  externals: {
+    react: 'react'
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx']
@@ -37,17 +41,18 @@ module.exports = {
     ]
   },
   optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          output: {
-            comments: false
-          }
-        },
-        extractComments: false
-      })
-    ]
+    // minimize: true,
+    // minimizer: [
+    //   new TerserPlugin({
+    //     exclude: /node_modules/,
+    //     terserOptions: {
+    //       output: {
+    //         comments: false
+    //       }
+    //     },
+    //     extractComments: false
+    //   })
+    // ]
   },
   plugins: [new CleanWebpackPlugin()]
 };
